2f144f12b35551f5a58e948b006262a11988732b
Nicolas <nicolasb813@gmail.com>
2024-04-22 18:16:51 -0300

    Sending gameplay events to make differents transitions on State Tree

--- Source/AIOverviewThesis/AIControllers/StateTreeAIController.cpp
+++ Source/AIOverviewThesis/AIControllers/StateTreeAIController.cpp
@@ -3,8 +3,24 @@
 
 #include "StateTreeAIController.h"
 
+#include "AIOverviewThesis/AIHelpers/AIHelpersLibrary.h"
+#include "AIOverviewThesis/Characters/AIStateTreeCharacter.h"
+#include "Components/StateTreeComponent.h"
+
 
 void AStateTreeAIController::PerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
 {
 	Super::PerceptionUpdated(Actor, Stimulus);
+
+	const AAIStateTreeCharacter* StateTreeCharacter = CastChecked<AAIStateTreeCharacter>(GetCharacter());
+	UStateTreeComponent* StateTreeComponent = StateTreeCharacter->GetStateTreeComponent();
+	
+	if (Stimulus.WasSuccessfullySensed())
+	{
+		StateTreeComponent->SendStateTreeEvent(TAG_AI_STATES_MOVETO_PLAYER);
+	}
+	else
+	{
+		StateTreeComponent->SendStateTreeEvent(TAG_AI_STATES_MOVETO_LASTSTIMULUSLOCATION);
+	}
 }
